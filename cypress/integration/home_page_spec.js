21220fce50b05ff9973a7f337d7c88ae80eb2da9
saulelabra <quiquelabra@gmail.com>
2019-11-07 19:42:59 -0600

    test update

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,41 @@
-describe('The Home Page', function() {
+describe('Test home page', function() {
     it('successfully loads', function() {
-      cy.visit('http://localhost:3000') // change URL to match your dev URL
+      cy.visit('/') // change URL to match your dev URL
+    })
+  })
+
+  describe('Test Login', function() {
+    it('successfully loads', function() {
+      cy.visit('/Login') // change URL to match your dev URL
+    })
+  })
+
+  describe('Test MyFiles', function() {
+    it('successfully loads', function() {
+      cy.visit('/MyFiles') // change URL to match your dev URL
+    })
+  })
+
+  describe('Test QR', function() {
+    it('successfully loads', function() {
+      cy.visit('/QR') // change URL to match your dev URL
+    })
+  })
+
+  describe('Test QR_not_user', function() {
+    it('successfully loads', function() {
+      cy.visit('/QR_not_user') // change URL to match your dev URL
+    })
+  })
+
+  describe('Test Signup', function() {
+    it('successfully loads', function() {
+      cy.visit('/Signup') // change URL to match your dev URL
+    })
+  })
+
+  describe('Test DeleteAccount', function() {
+    it('successfully loads', function() {
+      cy.visit('/DeleteAccount') // change URL to match your dev URL
     })
   })
